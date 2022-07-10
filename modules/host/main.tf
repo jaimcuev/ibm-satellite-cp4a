2660fa96691c241410723e6d33261e6c68991e4c
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-10 18:29:18 -0500

    cambios

--- modules/host/main.tf
+++ modules/host/main.tf
@@ -43,7 +43,6 @@ locals {
   volumes = flatten([
     for index, vsi in var.vsi: flatten([
       for volume in vsi.volumes: [{
-        zone: vsi.zone
         instance = ibm_is_instance.vpc_host_vsi[index].id
         capacity = volume
       }]
@@ -57,7 +56,7 @@ resource "ibm_is_instance_volume_attachment" "example" {
   instance = element(local.volumes, count.index).instance
 
   name = "att-${var.project}-${var.type}-${var.environment}-${format("%03s", count.index + 1)}"
-  iops = 100
+  profile = "10iops-tier"
   capacity = element(local.volumes, count.index).capacity
   delete_volume_on_attachment_delete = true
   delete_volume_on_instance_delete = true
