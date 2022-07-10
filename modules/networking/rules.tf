d382c8b64ac242e0f66ccc1dfca5486ce57921bf
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-09 23:02:23 -0500

    cambios

--- modules/networking/rules.tf
+++ modules/networking/rules.tf
@@ -1,5 +1,9 @@
+data "ibm_is_security_group" "vpc_security_group" {
+  name = ibm_is_vpc.vpc_vm.security_group[0].group_name
+}
+
 resource "ibm_is_security_group_rule" "outbound_tcp_3389_3389" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "outbound"
   tcp {
     port_min = 3389
@@ -8,7 +12,7 @@ resource "ibm_is_security_group_rule" "outbound_tcp_3389_3389" {
 }
 
 resource "ibm_is_security_group_rule" "outbound_tcp_443_443" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "outbound"
   tcp {
     port_min = 443
@@ -17,7 +21,7 @@ resource "ibm_is_security_group_rule" "outbound_tcp_443_443" {
 }
 
 resource "ibm_is_security_group_rule" "outbound_udp_30000_32767" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "outbound"
   udp {
     port_min = 30000
@@ -26,7 +30,7 @@ resource "ibm_is_security_group_rule" "outbound_udp_30000_32767" {
 }
 
 resource "ibm_is_security_group_rule" "outbound_tcp_30000_32767" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "outbound"
   tcp {
     port_min = 30000
@@ -35,7 +39,7 @@ resource "ibm_is_security_group_rule" "outbound_tcp_30000_32767" {
 }
 
 resource "ibm_is_security_group_rule" "outbound_tcp_80_80" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "outbound"
   tcp {
     port_min = 80
@@ -44,7 +48,7 @@ resource "ibm_is_security_group_rule" "outbound_tcp_80_80" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_tcp_22_22" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   tcp {
     port_min = 22
@@ -53,7 +57,7 @@ resource "ibm_is_security_group_rule" "inbound_tcp_22_22" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_icmp" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   icmp {
     type = 8
@@ -61,7 +65,7 @@ resource "ibm_is_security_group_rule" "inbound_icmp" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_udp_30000_32767" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   udp {
     port_min = 30000
@@ -70,7 +74,7 @@ resource "ibm_is_security_group_rule" "inbound_udp_30000_32767" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_tcp_443_443" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   tcp {
     port_min = 443
@@ -79,7 +83,7 @@ resource "ibm_is_security_group_rule" "inbound_tcp_443_443" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_tcp_3389_3389" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   tcp {
     port_min = 3389
@@ -88,7 +92,7 @@ resource "ibm_is_security_group_rule" "inbound_tcp_3389_3389" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_tcp_30000_32767" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   tcp {
     port_min = 30000
@@ -97,7 +101,7 @@ resource "ibm_is_security_group_rule" "inbound_tcp_30000_32767" {
 }
 
 resource "ibm_is_security_group_rule" "inbound_tcp_80_80" {
-  group = ibm_is_security_group.vpc_security_group.id
+  group = data.ibm_is_security_group.vpc_security_group.id
   direction = "inbound"
   tcp {
     port_min = 80
