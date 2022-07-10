c21265716752682e75ab92daee4a1908b8998d6e
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-10 01:36:16 -0500

    cambios

--- instance.tf
+++ instance.tf
@@ -7,8 +7,8 @@ resource "tls_private_key" "private_key" {
   rsa_bits  = 4096
 }
 
-# resource "ibm_is_ssh_key" "rhel_ssh_key" {
-#   name = "ssh-${var.project}-${var.environment}-001"
-#   public_key = var.rhel_public_ssh_key != null ? var.rhel_public_ssh_key : tls_private_key.private_key.public_key_openssh
-#   resource_group = data.ibm_resource_group.resource_group.id
-# }
+resource "ibm_is_ssh_key" "rhel_ssh_key" {
+  name = "ssh-${var.project}-${var.environment}-001"
+  public_key = tls_private_key.private_key.public_key_openssh
+  resource_group = data.ibm_resource_group.resource_group.id
+}
