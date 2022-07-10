07a50f6d0038f42c632638232d84530518727244
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-09 23:58:55 -0500

    cambios

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "ibm_is_ssh_key" "rhel_ssh_key" {
   name = "ssh-key-${var.project}-${var.environment}-001"
   public_key = var.rhel_public_ssh_key
+  resource_group = data.ibm_resource_group.resource_group.id
 }
