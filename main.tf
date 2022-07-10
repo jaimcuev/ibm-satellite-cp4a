af822888522b0bdc073cfc1e5ae70caef4158159
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-09 21:53:56 -0500

    First commit

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
 data "ibm_resource_group" "resourceGroup" {
   name = var.resource_group_name
 }
-
-resource "ibm_resource_instance" "cos_instance" {
-  name              = "cos-instance-demo"
-  resource_group_id = data.ibm_resource_group.resourceGroup.id
-  service           = "cloud-object-storage"
-  plan              = "standard"
-  location          = "global"
-}
