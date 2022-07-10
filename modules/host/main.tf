d223d5afe2a58d03a57dd0d576785e0621d2b62a
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-10 18:23:37 -0500

    cambios

--- modules/host/main.tf
+++ modules/host/main.tf
@@ -6,26 +6,14 @@ data "ibm_resource_group" "resource_group" {
   name = var.resource_group
 }
 
-locals {
-  volumes = flatten([
-    for index, vsi in var.vsi: flatten([
-      for volume in vsi.volumes: [{
-        zone: vsi.zone
-        vsi_index = index
-        capacity = volume
-      }]
-    ])
-  ])
-}
-
-resource "ibm_is_volume" "vpc_worker_volume" {
-  count = length(local.volumes)
-  name = "volumen-${var.project}-${var.type}-${var.environment}-${format("%03s", count.index + 1)}"
-  profile = "10iops-tier"
-  capacity = element(local.volumes, count.index).capacity
-  resource_group = data.ibm_resource_group.resource_group.id
-  zone = element(local.volumes, count.index).zone
-}
+# resource "ibm_is_volume" "vpc_worker_volume" {
+#   count = length(local.volumes)
+#   name = "volumen-${var.project}-${var.type}-${var.environment}-${format("%03s", count.index + 1)}"
+#   profile = "10iops-tier"
+#   capacity = element(local.volumes, count.index).capacity
+#   resource_group = data.ibm_resource_group.resource_group.id
+#   zone = element(local.volumes, count.index).zone
+# }
 
 resource "ibm_is_instance" "vpc_host_vsi" {
   count = length(var.vsi)
@@ -39,12 +27,42 @@ resource "ibm_is_instance" "vpc_host_vsi" {
   zone = element(var.vsi, count.index).zone
   keys = [var.ssh_key_id]
 
-  volumes = slice(ibm_is_volume.vpc_worker_volume.*.id, length(element(var.vsi, count.index).volumes) * count.index, length(element(var.vsi, count.index).volumes) * count.index + length(element(var.vsi, count.index).volumes) - 1)
-
   primary_network_interface {
     subnet = var.vpc_subnets[index(var.vpc_subnets.*.zone, element(var.vsi, count.index).zone)].id
     allow_ip_spoofing = false
   }
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
+
+locals {
+  volumes = flatten([
+    for index, vsi in var.vsi: flatten([
+      for volume in vsi.volumes: [{
+        zone: vsi.zone
+        instance = ibm_is_instance.vpc_host_vsi[index].id
+        capacity = volume
+      }]
+    ])
+  ])
+}
+
+resource "ibm_is_instance_volume_attachment" "example" {
+  count = length(local.volumes)
+
+  instance = element(local.volumes, count.index).instance
+
+  name = "att-${var.project}-${var.type}-${var.environment}-${format("%03s", count.index + 1)}"
+  iops = 100
+  capacity = element(local.volumes, count.index).capacity
+  zone = element(local.volumes, count.index).zone
+  delete_volume_on_attachment_delete = true
+  delete_volume_on_instance_delete = true
+  volume_name = "vol-${var.project}-${var.type}-${var.environment}-${format("%03s", count.index + 1)}"
 
   timeouts {
     create = "15m"
