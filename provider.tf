fa3b8496202fb689c12cb9051eea1f74d0def4e9
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-09 22:03:29 -0500

    First commit

--- provider.tf
+++ provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    ibm = {
-      source = "IBM-Cloud/ibm"
-      version = "1.43.0"
-    }
-  }
-}
-
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
