7fe12c6e23f3909148b522ff25b4851bf21ed88f
Jaime Cueva Sánchez <jaimcuev@gmail.com>
2022-07-10 20:06:11 -0500

    cambios

--- host.tf
+++ host.tf
@@ -8,8 +8,4 @@ module "satellite-host" {
   location = module.satellite-location.location_id
   host_vms = module.controlplane.instances
   location_zones = var.satellite_location_zones
-
-  depends_on = [
-    module.controlplane
-  ]
 }
